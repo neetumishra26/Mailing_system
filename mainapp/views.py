f4ea0cb1dcbd8893d85e3fb8a0ca833fbf76f055
Neetu Mishra <neetumis@thoughtworks.com>
2011-12-17 11:39:33 +0530

    Separated template from view

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,14 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.template import Context
+from django.template.loader import get_template
 
 def main_page(request):
-    output = u'''
-        <html>
-            <head><title>%s</title></head>
-            <body>
-                <h1>%s</h1><p>%s</p>
-            </body>
-        </html>
-        ''' % (u'Mailing System', u'Welcome to Gmail', u'Here you can receive & send emails')
+    template = get_template('main_page.html')
+    variables = Context({
+        'head_title':u'Mailing System',
+        'page_title':u'Welcome to Gmail',
+        'page_body': u'Here you can receive & send emails'
+    })
+    output = template.render(variables)
     return HttpResponse(output)
