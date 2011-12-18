609a9cb422a41279b05f48b7f0b69c4ed09301f3
Neetu Mishra <neetumis@thoughtworks.com>
2011-12-18 21:19:15 +0530

    Refactoring in view of login, Added post method, & username is coming now

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,17 +1,31 @@
 # Create your views here.
-from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+#from django.forms.forms import Form
+from django.http import HttpResponseRedirect, HttpResponse
 from django.template import Context
 from django.shortcuts import render_to_response
 from django.contrib.auth import logout
+from django.template.context import RequestContext
 
 def main_page(request):
-    variables = Context({
-        'head_title':u'Mailing System',
-        'page_title':u'Welcome to NewMail',
-        'page_body': u'Here you can receive & send emails',
-        'user':request.user
-    })
-    return render_to_response('main_page.html',variables)
+    if request.method == 'POST':
+        #form = Form(request.POST)
+        current_user = User.objects.get(username=request.POST['username'])
+        if current_user.password == request.POST['password']:
+            variables = Context({
+                'head_title': u'Mailing System',
+                'page_title': u'Welcome to NewMail',
+                'page_body': u'Here you can receive & send emails',
+                'user_name': current_user.username
+            })
+            return render_to_response('main_page.html', variables)
+        else:
+            return render_to_response('registration/login.html',
+                    {'error_message': "Your username and password didn't match. Please try again.",
+                     'user_name': current_user.username
+                #    , 'form':form
+                },
+                     context_instance=RequestContext(request))
 
 def logout_page(request):
     logout(request)
