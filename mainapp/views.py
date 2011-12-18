99443adc744473c5ad09fbfa5da81a2abdc1830f
Neetu Mishra <neetumis@thoughtworks.com>
2011-12-18 23:23:27 +0530

    Added compose mail module

--- mainapp/views.py
+++ mainapp/views.py
@@ -6,18 +6,14 @@ from django.template import Context
 from django.shortcuts import render_to_response
 from django.contrib.auth import logout
 from django.template.context import RequestContext
+from mainapp.models import Email
 
 def main_page(request):
-    if request.method == 'GET':
-        return render_to_response('main_page.html')
     if request.method == 'POST':
         #form = Form(request.POST)
         current_user = User.objects.get(username=request.POST['username'])
         if current_user.password == request.POST['password']:
             variables = Context({
-                'head_title': u'Mailing System',
-                'page_title': u'Welcome to NewMail',
-                'page_body': u'Here you can receive & send emails',
                 'user_name': current_user.username
             })
             return render_to_response('main_page.html', variables)
@@ -28,10 +24,18 @@ def main_page(request):
                 #    , 'form':form
                 },
                      context_instance=RequestContext(request))
+    return render_to_response('main_page.html')
 
 def logout_page(request):
     logout(request)
     return HttpResponseRedirect('/')
 
 def compose_mail(request):
+    if request.method == 'POST':
+        current_user = User.objects.get(username=request.POST['username'])
+        composed_email = Email(to_email_id=request.POST['to_id'],from_email_id=current_user.email,subject=request.POST['subject'],content=request.POST['content'])
+        composed_email.save()
+        return render_to_response('main_page.html', Context({
+                'user_name': current_user.username,
+                'success_message': 'Your Email sent successfully'}))
     return render_to_response('project/compose_mail.html')
