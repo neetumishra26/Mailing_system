e00de464f6213b845871a71062a9295f496b5a0a
Neetu Mishra <neetumis@thoughtworks.com>
2011-12-18 20:03:35 +0530

    Removed login folder

--- settings.py
+++ settings.py
@@ -141,7 +141,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mailing_system.mainapp',
-    'mailing_system.login',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
