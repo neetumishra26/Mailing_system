feee1cdf944767ccf5c7bcc164ca9c9619b54ccd
Neetu Mishra <neetumis@thoughtworks.com>
2011-12-18 19:47:24 +0530

    Added view for logout & code refactoring but username is still not getting into view:(

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,18 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.template import Context
-from django.template.loader import get_template
+from django.shortcuts import render_to_response
+from django.contrib.auth import logout
 
 def main_page(request):
-    template = get_template('main_page.html')
     variables = Context({
         'head_title':u'Mailing System',
-        'page_title':u'Welcome to Gmail',
-        'page_body': u'Here you can receive & send emails'
+        'page_title':u'Welcome to NewMail',
+        'page_body': u'Here you can receive & send emails',
+        'user':request.user
     })
-    output = template.render(variables)
-    return HttpResponse(output)
+    return render_to_response('main_page.html',variables)
+
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect('/')
--- urls.py
+++ urls.py
@@ -1,16 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
-from mainapp.views import main_page
+from mainapp.views import main_page, logout_page
 
 urlpatterns = patterns('',
     (r'^$', main_page),
     (r'^login/$', 'django.contrib.auth.views.login' ),
-    # Examples:
-    # url(r'^$', 'mailing_system.views.home', name='home'),
-    # url(r'^mailing_system/', include('mailing_system.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    (r'^logout/$', logout_page ),
 )
